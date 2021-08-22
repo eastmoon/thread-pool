6f87965edee434fabaa8f199e6b2d8f569756825
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-08-22 23:26:09 +0800

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -3,4 +3,8 @@
 // 引入 UTF header，注意執行環境差異需選用不同的 header
 #include <boost/test/included/unit_test.hpp>
 // 測試模組
-#include "./case/test_1.cpp"
+//#include "./case/test_thread_worker_count.cpp"
+//#include "./case/test_jobpool_worker_controller_atomic_count.cpp"
+//#include "./case/test_jobpool_worker_controller_nonlock_count.cpp"
+//#include "./case/test_jobpool_pool_atomic_count.cpp"
+//#include "./case/test_jobpool_pool_nonlock_count.cpp"
